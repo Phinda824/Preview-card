f781e21e7887314ad73550e4ef7dc76619c07566
Phinda Simanga <simanga824@gmail.com>
2024-06-12 22:20:43 +0200

    done

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let time2 = document.getElementById('time-2');
 let time3 = document.getElementById('time-3');
 let time4 = document.getElementById('time-4');
 
+let cover = document.getElementById('cover');
+let ingredients = document.getElementById('ingredients');
+let link = document.getElementById('ingre-link');
+
+
 time2.addEventListener('click', () => {
     time2.classList.add('current');
     time1.classList.remove('current');
@@ -27,3 +32,6 @@ time4.addEventListener('click', () => {
     time3.classList.remove('current');
     time1.classList.remove('current');
 })
+
+
+
