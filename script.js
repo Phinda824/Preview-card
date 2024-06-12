c9b76323bbd9ed82cd915b03dd41d07bbd9ef911
Phinda Simanga <simanga824@gmail.com>
2024-06-12 21:31:32 +0200

    updates

---
+++ script.js
@@ -0,0 +1,29 @@
+let time1 = document.getElementById('time-1');
+let time2 = document.getElementById('time-2');
+let time3 = document.getElementById('time-3');
+let time4 = document.getElementById('time-4');
+
+time2.addEventListener('click', () => {
+    time2.classList.add('current');
+    time1.classList.remove('current');
+    time3.classList.remove('current');
+    time4.classList.remove('current');
+})
+time1.addEventListener('click', () => {
+    time1.classList.add('current');
+    time2.classList.remove('current');
+    time3.classList.remove('current');
+    time4.classList.remove('current');
+})
+time3.addEventListener('click', () => {
+    time3.classList.add('current');
+    time2.classList.remove('current');
+    time1.classList.remove('current');
+    time4.classList.remove('current');
+})
+time4.addEventListener('click', () => {
+    time4.classList.add('current');
+    time2.classList.remove('current');
+    time3.classList.remove('current');
+    time1.classList.remove('current');
+})
